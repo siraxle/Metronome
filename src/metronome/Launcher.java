341b3998b790350a9f31687c8e8e65bf0fa475fb
siraxle <sir.axle@yandex.ru>
2022-02-12 12:19:16 +0300

    refactor class Launcher

--- src/metronome/Launcher.java
+++ src/metronome/Launcher.java
@@ -17,63 +17,28 @@ import java.util.concurrent.TimeUnit;
 public class Launcher extends Application {
   Scene scene;
   BorderPane root;
-  int i = 0;
-  boolean toExit;
+  int clickAmount = 0;
   Button button;
   Text bpmLabel;
   TextField bpmInput;
+  boolean isPlaying;
 
   public static void main(String[] args) {
     launch(args);
   }
 
-  EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
+  EventHandler<ActionEvent> buttonHandler = new EventHandler<ActionEvent>() {
     @Override
     public void handle(ActionEvent event) {
-      String[] strings = event.getTarget().toString().split("'");
-      String buttonName = strings[1];
-      System.out.println(buttonName);
-      if (buttonName.equals("START") && !bpmInput.getText().isEmpty()) {
-        toExit = false;
-        try {
-          Integer.parseInt(bpmInput.getText());
-          button.setText("STOP");
-        } catch (NumberFormatException e) {
-          System.out.println("BPM не число");
-        }
-        Thread clicking = new Thread(new Runnable() {
-          @Override
-          public void run() {
-            File file = new File("/Users/efremov/Documents/java_projects/metronome/assets/clickSimple.wav");
-            Sound sound = new Sound(file);
-            while (!toExit) {
-              i++;
-              try {
-                long time = 60000 / Long.parseLong(bpmInput.getText());
-                System.out.println(time);
-                try {
-                  TimeUnit.MILLISECONDS.sleep(time);
-                } catch (InterruptedException e) {
-                  e.printStackTrace();
-                }
-                System.out.println(i);
-                sound.play();
-//                sound.playSound("assets/clickSimple.wav");
-              } catch (NumberFormatException e) {
-                System.out.println("Введите числовое значение BPM");
-                bpmInput.clear();
-                bpmInput.setPromptText("Введите числовое значение BPM");
-                break;
-              }
-
-            }
-          }
-        });
-        clicking.start();
+      int bpmValue = validatedBpmValue();
+      if (isPlaying) {
+        isPlaying = false;
+        button.setText("STOP");
+        startClicking(bpmValue);
       } else {
         button.setText("START");
-        toExit = true;
-        i = 0;
+        isPlaying = true;
+        clickAmount = 0;
       }
     }
   };
@@ -81,28 +46,82 @@ public class Launcher extends Application {
   @Override
   public void start(Stage primaryStage) {
     primaryStage.setTitle("Metronome");
+    setTextField();
+    setBpmLable();
+    setLaunchButton();
+    this.scene = new Scene(root, 300, 300);
+    primaryStage.setScene(this.scene);
+    primaryStage.show();
+  }
 
+  private void setLaunchButton() {
+    //Enter button to the right side of text field
+    button = new Button();
+    button.setText("START");
+    this.root.setTop(button);
+    this.root.setAlignment(button, Pos.CENTER);
+    button.setOnAction(this.buttonHandler);
+  }
+
+  private void setBpmLable() {
+    //Enter "BPM" text to the left side of text field
+    bpmLabel = new Text("BPM");
+    this.root.setLeft(bpmLabel);
+    this.root.setAlignment(bpmLabel, Pos.CENTER_LEFT);
+  }
+
+  private void setTextField() {
     //Enter text field
     bpmInput = new TextField();
     bpmInput.setPromptText("Enter BPM");
     bpmInput.setFocusTraversable(false);
     this.root = new BorderPane();
     this.root.setCenter(bpmInput);
-
-    //Enter "BPM" text to the left side of text field
-    bpmLabel = new Text("BPM");
-    this.root.setLeft(bpmLabel);
-    this.root.setAlignment(bpmLabel, Pos.CENTER_LEFT);
-
-    //Enter button to the right side of text field
-    button = new Button();
-    button.setText("START");
-    this.root.setTop(button);
-    this.root.setAlignment(button, Pos.CENTER);
-    button.setOnAction(this.handler);
-
-    this.scene = new Scene(root, 300, 300);
-    primaryStage.setScene(this.scene);
-    primaryStage.show();
   }
+
+  private int validatedBpmValue() {
+    String bpmInputText = bpmInput.getText();
+    int bpmValue = 0;
+    if (bpmInputText.isEmpty()) {
+      throw new IllegalArgumentException("empty value");
+    }
+      bpmValue = Integer.parseInt(bpmInputText);
+    if (bpmValue <= 0) {
+      throw new IllegalArgumentException("Negative value");
+    }
+    return bpmValue;
+  }
+
+  private void startClicking(int bpmValue){
+    Thread clicking = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        File file = new File("assets/clickSimple.wav");
+        Sound sound = new Sound(file);
+        while (!isPlaying) {
+          clickAmount++;
+          try {
+            long time = 60000 / bpmValue;
+            System.out.println(time);
+            try {
+              TimeUnit.MILLISECONDS.sleep(time);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+            System.out.println(clickAmount);
+            sound.play();
+//                sound.playSound("assets/clickSimple.wav");
+          } catch (NumberFormatException e) {
+            System.out.println("Введите числовое значение BPM");
+            bpmInput.clear();
+            bpmInput.setPromptText("Введите числовое значение BPM");
+            break;
+          }
+
+        }
+      }
+    });
+    clicking.start();
+  }
+
 }
