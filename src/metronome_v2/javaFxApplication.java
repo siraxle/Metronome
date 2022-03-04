95dd3710835bee48a4f500c861d7441f6a789cf4
siraxle <sir.axle@yandex.ru>
2022-03-05 01:13:10 +0300

    add time signature choice

--- src/metronome_v2/javaFxApplication.java
+++ src/metronome_v2/javaFxApplication.java
@@ -6,14 +6,13 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import metronome.Sound;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class javaFxApplication extends Application {
 
@@ -21,7 +20,8 @@ public class javaFxApplication extends Application {
   private TextField bpmInput;
   private int clickAmount = 0;
   private Metronome_v2 metronome;
-
+  private ChoiceBox<String> choiceBoxes;
+  private HBox checkBoxArea;
 
   public static void main(String[] args) {
     launch(args);
@@ -35,22 +35,26 @@ public class javaFxApplication extends Application {
     primaryStage.setScene(scene);
     primaryStage.show();
     initComponents(panel);
-    setHandlers();
+    setButtonHandlers();
+    setChoiceBoxes();
+    setChoiceHandler();
   }
 
   private void initComponents(Parent panel) {
     this.launchButton = (Button) panel.lookup("#launchButton");
     this.bpmInput = (TextField) panel.lookup("#bpmInput");
+    this.choiceBoxes = (ChoiceBox<String>) panel.lookup("#time_signature");
+    this.checkBoxArea = (HBox) panel.lookup("#strongBeatsArea");
   }
 
-  private void setHandlers() {
+  private void setButtonHandlers() {
     EventHandler<ActionEvent> buttonHandler = new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent event) {
         if (metronome != null && metronome.isPlaying) {
           metronome.stop();
           launchButton.setText("START");
-        }else {
+        } else {
           int bpm = 0;
           try {
             bpm = Integer.parseInt(bpmInput.getText());
@@ -67,8 +71,26 @@ public class javaFxApplication extends Application {
     launchButton.setOnAction(buttonHandler);
   }
 
+  private void setChoiceHandler() {
+    EventHandler<ActionEvent> choiceHandler = new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        printCheckBoxArea();
+      }
+    };
+    choiceBoxes.setOnAction(choiceHandler);
+  }
 
+  private void setChoiceBoxes() {
+    String[] signatures = {"3/4", "4/4", "5/4", "7/4", "9/4"};
+    choiceBoxes.setValue("4/4");
+    choiceBoxes.getItems().addAll(signatures);
+  }
 
-
+  private void printCheckBoxArea() {
+    //signature will never be more than 10
+    String amountCheckBoxes = choiceBoxes.getValue().substring(0, 1);
+    System.out.println(amountCheckBoxes);
+  }
 
 }
