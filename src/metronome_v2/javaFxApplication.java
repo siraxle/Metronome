50ca110ab41daa2eaf2fc61f80b27f3e661f42a2
siraxle <sir.axle@yandex.ru>
2022-03-08 02:08:17 +0300

    add dynamic view check boxes in signature area

--- src/metronome_v2/javaFxApplication.java
+++ src/metronome_v2/javaFxApplication.java
@@ -4,13 +4,13 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 
 import java.io.IOException;
 
@@ -20,8 +20,9 @@ public class javaFxApplication extends Application {
   private TextField bpmInput;
   private int clickAmount = 0;
   private Metronome_v2 metronome;
-  private ChoiceBox<String> choiceBoxes;
-  private HBox checkBoxArea;
+  private ComboBox<String> choiceBoxes;
+  private HBox checkBoxArea = new HBox(10);
+//  private Label label = new Label();
 
   public static void main(String[] args) {
     launch(args);
@@ -33,6 +34,7 @@ public class javaFxApplication extends Application {
     Scene scene = new Scene(panel, 400, 440);
     primaryStage.setTitle("Metronome");
     primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
     primaryStage.show();
     initComponents(panel);
     setButtonHandlers();
@@ -43,8 +45,9 @@ public class javaFxApplication extends Application {
   private void initComponents(Parent panel) {
     this.launchButton = (Button) panel.lookup("#launchButton");
     this.bpmInput = (TextField) panel.lookup("#bpmInput");
-    this.choiceBoxes = (ChoiceBox<String>) panel.lookup("#time_signature");
+    this.choiceBoxes = (ComboBox<String>) panel.lookup("#time_signature");
     this.checkBoxArea = (HBox) panel.lookup("#strongBeatsArea");
+//    this.label = (Label) panel.lookup("#lableHBox");
   }
 
   private void setButtonHandlers() {
@@ -83,13 +86,21 @@ public class javaFxApplication extends Application {
 
   private void setChoiceBoxes() {
     String[] signatures = {"3/4", "4/4", "5/4", "7/4", "9/4"};
-    choiceBoxes.setValue("4/4");
+//    choiceBoxes.setValue("4/4");
     choiceBoxes.getItems().addAll(signatures);
   }
 
   private void printCheckBoxArea() {
     //signature will never be more than 10
+    checkBoxArea.setPadding(new Insets(10, 50, 10, 80));
+    if (checkBoxArea.getChildren().size() != 0) {
+      checkBoxArea.getChildren().clear();
+    }
     String amountCheckBoxes = choiceBoxes.getValue().substring(0, 1);
+    for (int i = 0; i < Integer.parseInt(amountCheckBoxes); i++) {
+      CheckBox checkBox = new CheckBox();
+      checkBoxArea.getChildren().add(checkBox);
+    }
     System.out.println(amountCheckBoxes);
   }
 
